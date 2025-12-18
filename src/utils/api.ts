4c1c90058b3fe0016c037d09c17bc3f0215e8f56
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2025-12-18 15:42:08 +0530

    responce intercepter

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
@@ -14,61 +14,61 @@ const api = axios.create({
 //   (error) => Promise.reject(error)
 // );
 
-// api.interceptors.response.use(
-//   (response) => response,
+api.interceptors.response.use(
+  (response) => response,
 
-//   async (error) => {
-//     const { config } = error;
+  async (error) => {
+    const { config } = error;
 
-//     // Prevent infinite retry loop
-//     if (config._retry) {
-//       return Promise.reject(error);
-//     }
+    // Prevent infinite retry loop
+    if (config._retry) {
+      return Promise.reject(error);
+    }
 
-//     // --------------------------
-//     // 1️⃣ ACCESS TOKEN EXPIRED
-//     // --------------------------
-//     if (
-//       error.response?.data?.status === 401 &&
-//       error.response?.data?.message === "UNAUTHORIZED"
-//     ) {
-//       config._retry = true;
+    // --------------------------
+    // 1️⃣ ACCESS TOKEN EXPIRED
+    // --------------------------
+    if (
+      error.response?.data?.status === 401 &&
+      error.response?.data?.message === "UNAUTHORIZED"
+    ) {
+      config._retry = true;
 
-//       try {
-//         // attempt refresh token
-//         await api.get("/auth/refresh");
+      try {
+        // attempt refresh token
+        await api.get("/auth/refresh");
 
-//         // retry the original request
-//         return api(config);
-//       } catch (err) {
-//         const refreshErr = err as AxiosError<{
-//           status: number;
-//           message: string;
-//         }>;
+        // retry the original request
+        return api(config);
+      } catch (err) {
+        const refreshErr = err as AxiosError<{
+          status: number;
+          message: string;
+        }>;
 
-//         // ------------------------------
-//         // 2️⃣ REFRESH TOKEN ALSO INVALID
-//         // ------------------------------
-//         if (
-//           refreshErr.response?.data?.status === 401 &&
-//           refreshErr.response?.data?.message === "UNAUTHORIZED_NO_REFRESH"
-//         ) {
-//           console.log("⛔ Refresh token expired → redirecting to login...");
+        // ------------------------------
+        // 2️⃣ REFRESH TOKEN ALSO INVALID
+        // ------------------------------
+        if (
+          refreshErr.response?.data?.status === 401 &&
+          refreshErr.response?.data?.message === "UNAUTHORIZED_NO_REFRESH"
+        ) {
+          console.log("⛔ Refresh token expired → redirecting to login...");
 
-//           // FORCE LOGOUT → Redirect to login page
-//           // window.location.href = "/login";
-//         }
+          // FORCE LOGOUT → Redirect to login page
+          // window.location.href = "/login";
+        }
 
-//         console.log("ResponseInterceptor:", refreshErr);
-//         return Promise.reject(refreshErr);
-//       }
-//     }
+        console.log("ResponseInterceptor:", refreshErr);
+        return Promise.reject(refreshErr);
+      }
+    }
 
-//     // --------------------------
-//     // 3️⃣ OTHER ERRORS
-//     // --------------------------
-//     return Promise.reject(error);
-//   }
-// );
+    // --------------------------
+    // 3️⃣ OTHER ERRORS
+    // --------------------------
+    return Promise.reject(error);
+  }
+);
 
 export default api;
