c9eda6f02477236288a6820fcbe76f24ebfdd472
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2026-01-24 00:00:30 +0530

    Comment out timeout in API axios instance
    Comment out the timeout setting in the API configuration.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,7 +5,7 @@ import { useAuthStore } from "../store/useAuthStore";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
   // baseURL: import.meta.env.MODE ==="development" ? import.meta.env.VITE_API_URL + "/api" : "/api", // e.g. "http://localhost:5000/api"
-  timeout: 10000,
+  // timeout: 10000,
   withCredentials: true, // cookies enabled
 });
 
